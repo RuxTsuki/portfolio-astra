afffd66b1bf9215a10b7aa273bb5a44a661a98a8
SaiKisho <diegosferas@gmail.com>
2023-12-12 18:19:46 -0500

    🌟  | Footer not ready yet

---
+++ src/components/react/ui/molecules/sections/footer/Footer.tsx
@@ -0,0 +1,27 @@
+
+export const Footer = () => {
+    return (
+        <div className="pt-[30px] relative">
+
+            <footer className="bg-[#091833] px-[15px] pt-[10px]">
+                <div className="container mx-auto">
+
+                    <div className="p-[25px]">
+                        <h3 className="text-[#fff] text-[2rem] font-bold">
+                            En construccion 🚧
+                        </h3>
+
+                    </div>
+
+
+                    <div className="flex justify-center">
+                        <p className="small-text-helper">
+                            &copy; Copyright 2023. Todos los derechos reservados.
+                        </p>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+    )
+}
